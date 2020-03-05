d3c780acd6b808846b814fd9ef7b2af79eca94e0
mathoian <marcelo.henrik@gmail.com>
2020-03-05 15:36:05 -0300

    menu+cadastrar surfista [implementado]

--- app/src/main/java/com/mclohrk/appcampeonatosurf/Listar_Surfista_Activity.java
+++ app/src/main/java/com/mclohrk/appcampeonatosurf/Listar_Surfista_Activity.java
@@ -2,9 +2,13 @@ package com.mclohrk.appcampeonatosurf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.AndroidException;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -27,9 +31,28 @@ public class Listar_Surfista_Activity extends AppCompatActivity {
         surfistaDAO = new SurfistaDAO(this);
         surfistas = surfistaDAO.listaSurfistas();
         SurfistasFiltrado.addAll(surfistas);
-        ArrayAdapter<Surfista> adapter = new ArrayAdapter<Surfista>(this, android.R.layout.simple_list_item_1,surfistas);
+        ArrayAdapter<Surfista> adapter = new ArrayAdapter<Surfista>(this, android.R.layout.simple_list_item_1, SurfistasFiltrado);
         listview.setAdapter(adapter);
 
+    }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    public void cadastrar(MenuItem item) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        surfistas = surfistaDAO.listaSurfistas();
+        SurfistasFiltrado.clear();
+        SurfistasFiltrado.addAll(surfistas);
+        listview.invalidate();
     }
 }
